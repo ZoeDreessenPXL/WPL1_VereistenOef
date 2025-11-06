b5e34854fa509d31e8fe22e6c79706a88790d17a
carinezoe <zoe.dreessen@student.pxl.be>
2025-11-06 13:15:16 +0100

    DEMO-02

--- Oefening/Oefening/Program.cs
+++ Oefening/Oefening/Program.cs
@@ -5,9 +5,12 @@
         static void Main(string[] args)
         {
             string userName;
+            string password;
 
             Console.Write("Enter username: ");
             userName = Console.ReadLine();
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
         }
     }
 }
